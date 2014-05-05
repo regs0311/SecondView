1bad0726369fa85e3de9224147fb52d12d20f369
eqii <eero.j.palomaki@aalto.fi>
2014-05-05 20:21:37 +0300

    Swiping works correctly

--- secondview/app/webroot/js/swipe.js
+++ secondview/app/webroot/js/swipe.js
@@ -2,10 +2,18 @@ $(document).ready(function() {
 
   $("#myCarousel").swiperight(function() {
     $("#myCarousel").carousel('prev');
+    $(".item").click(function () {return false;});
+    setTimeout(function() {
+       $(".item").unbind("click");   
+    }, 2500);
   });
 
   $("#myCarousel").swipeleft(function() {
     $("#myCarousel").carousel('next');
+    $(".item").click(function () {return false;});
+    setTimeout(function() {
+       $(".item").unbind("click");   
+    }, 2500);
   });
 
   $("#myCarousel").carousel({
@@ -15,7 +23,6 @@ $(document).ready(function() {
 
 
 });
-
 
 
 //A hack to prevent default drag event
